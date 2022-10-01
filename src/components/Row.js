a99682bc4cbcd3e02376b4e03ceababdf972e3c3
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-01 22:20:46 +0600

    icon added

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
 
 import Movie from './Movie';
 
@@ -24,7 +24,7 @@ const Row = ({ title, fetchURL, rowId }) => {
         <>
             <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
             <div className="relative flex items-center group">
-                <MdChevronLeft
+                <HiOutlineChevronLeft
                     onClick={slideLeft}
                     className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
                     size={40}
@@ -37,7 +37,7 @@ const Row = ({ title, fetchURL, rowId }) => {
                         <Movie key={id} item={item} />
                     ))}
                 </div>
-                <MdChevronRight
+                <HiOutlineChevronRight
                     onClick={slideRight}
                     className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
                     size={40}
--- src/components/SavedShow.js
+++ src/components/SavedShow.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
 import { UserAuth } from '../context/AuthContext';
 import { db } from '../firebase';
 import { updateDoc, doc, onSnapshot } from 'firebase/firestore';
@@ -40,7 +40,7 @@ const SavedShows = () => {
         <>
             <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
             <div className="relative flex items-center group">
-                <MdChevronLeft
+                <HiOutlineChevronLeft
                     onClick={slideLeft}
                     className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
                     size={40}
@@ -73,7 +73,7 @@ const SavedShows = () => {
                         </div>
                     ))}
                 </div>
-                <MdChevronRight
+                <HiOutlineChevronRight
                     onClick={slideRight}
                     className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
                     size={40}
