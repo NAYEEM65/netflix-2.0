00bede5b70f415ae894681812753df948dfdc1f7
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-01 16:18:54 +0600

    account page aded

---
+++ src/components/SavedShow.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+const SavedShow = () => {
+    const slideLeft = () => {
+        let slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    };
+    const slideRight = () => {
+        let slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    };
+    return (
+        <>
+            <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
+            <div className="relative flex items-center group">
+                <MdChevronLeft
+                    onClick={slideLeft}
+                    className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+                    size={40}
+                />
+                <div
+                    id={'slider' + rowId}
+                    className={`h-full w-full whitespace-nowrap overflow-x-scroll scroll-smooth scrollbar-hide relative`}
+                >
+                    {movies.map((item, id) => (
+                        <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[200px] inline-block cursor-pointer relative p-2">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                                alt={item?.title}
+                            />
+                            <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                                    {item?.title}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <MdChevronRight
+                    onClick={slideRight}
+                    className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+                    size={40}
+                />
+            </div>
+        </>
+    );
+};
+
+export default SavedShow;
